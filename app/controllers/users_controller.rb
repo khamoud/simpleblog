96a22f84d4f2fdef53488fe833a681d3b395a420
Kris Hamoud <khamoud617@gmail.com>
2013-06-26 16:52:35 -0700

    register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def index
   	@users = User.all
   end
+
+  def new
+  	
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	@message = Array.new
+  	if @user.save
+  		flash[:success] = "User successfully registered"
+  	else
+  		flash[:failure] = @user.errors.full_messages
+  	end
+  	render :new
+  end
+
 end
