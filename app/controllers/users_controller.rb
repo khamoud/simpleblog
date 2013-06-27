96ec640a05901fa163324decdd449cc37ab77d88
Kris Hamoud <khamoud617@gmail.com>
2013-06-26 17:34:31 -0700

    commit three

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,43 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
-  def new
-  	
-  end
-
   def create
   	@user = User.new(params[:user])
   	@message = Array.new
   	if @user.save
   		flash[:success] = "User successfully registered"
+  		redirect_to @user
   	else
   		flash[:failure] = @user.errors.full_messages
+  		render :new
   	end
-  	render :new
+  	
   end
 
+  def new
+  	@user = User.new
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update_attributes(params[:id])
+  		redirect_to @user
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to '/users/'
+  end
 end
